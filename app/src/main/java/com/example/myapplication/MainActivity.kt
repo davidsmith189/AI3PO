fd28f7eb0b05982541d73817e81164a77507c835
gabrielesq11 <gabrielesq11@gmail.com>
2025-03-12 22:16:30 -0700

    final test

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
@@ -14,8 +15,17 @@ class MainActivity : AppCompatActivity() {
         val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
         val viewPager = findViewById<ViewPager2>(R.id.viewPager)
 
+
+
         val adapter = ViewPagerAdapter(this)
         viewPager.adapter = adapter
+
+        val toolbar: Toolbar = findViewById(R.id.custom_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+
 
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             when (position) {
