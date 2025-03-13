fbd108aaf9d1b7c1e987a7b3270121265a06f98b
gabrielesq11 <gabrielesq11@gmail.com>
2025-03-13 01:37:39 -0700

    final commit for ui sprint 1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,27 +1,36 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar= findViewById<androidx.appcompat.widget.Toolbar>(R.id.custom_toolbar)
+        setSupportActionBar(toolbar)
+
         val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
         val viewPager = findViewById<ViewPager2>(R.id.viewPager)
 
+        val saveIcon: ImageView = findViewById(R.id.save)
+        saveIcon.setOnClickListener {
+            Toast.makeText(this, "Chat Saved Successfully", Toast.LENGTH_SHORT).show()
+        }
 
 
         val adapter = ViewPagerAdapter(this)
         viewPager.adapter = adapter
 
-        val toolbar: Toolbar = findViewById(R.id.custom_toolbar)
-        setSupportActionBar(toolbar)
+
 
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
