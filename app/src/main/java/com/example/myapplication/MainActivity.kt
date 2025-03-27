c05b226e73ca8949052d49205067fee8e214c233
rafaeldiaz619 <rafael.diaz619@myci.csuci.edu>
2025-03-26 23:40:17 -0700

    updated master

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import android.widget.TextView
 import com.google.firebase.firestore.FirebaseFirestore
 
 
